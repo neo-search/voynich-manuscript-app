2abba83c0ec6705afddca6926b810879ea6276b0
Daniel Beck <d.danielbeck@googlemail.com>
2021-09-11 11:58:09 +0200

    styling for thumbnails

--- components/PageThumbnail.tsx
+++ components/PageThumbnail.tsx
@@ -1,14 +1,27 @@
 import * as React from "react";
 
-import { Text, TextProps } from "./Themed";
+import { TextProps, TextLight } from "./Themed";
 import { Image } from "react-native-elements";
 import { PageProps } from "../services/DocumentService";
+import { View, Text } from "react-native";
 
 export function PageThumbnail({ page }: { page: PageProps }) {
   return (
-    <Image
-      style={{ width: 90, height: 120, margin: 8}}
-      source={{ uri: page.imageUrl }}
-    ></Image>
+    <View style={{ flexDirection: "column", margin: 8 }}>
+      <View
+        style={{
+          shadowColor: "#524934",
+          shadowOffset: { width: 4, height: 4 },
+          shadowOpacity: 0.4,
+          shadowRadius: 4,
+        }}
+      >
+        <Image
+          style={{ width: 90, height: 120 }}
+          source={{ uri: page.imageUrl }}
+        ></Image>
+      </View>
+      <TextLight style={{ marginTop: 5 }}>{page.pageTitle}</TextLight>
+    </View>
   );
 }
