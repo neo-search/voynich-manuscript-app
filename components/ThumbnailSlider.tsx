d85eea505c871ad6be3247f8f22832c6455feee8
Daniel Beck <d.danielbeck@googlemail.com>
2021-09-11 13:48:17 +0200

    thumbnail slider scrollable

--- components/ThumbnailSlider.tsx
+++ components/ThumbnailSlider.tsx
@@ -1,17 +1,26 @@
 import * as React from "react";
-
-import { Text, TextProps } from "./Themed";
-import { Image } from "react-native-elements";
+import { FlatList } from "react-native";
 import { PageProps } from "../services/DocumentService";
 import { PageThumbnail } from "./PageThumbnail";
-import { View } from "react-native";
+
+
+const renderItem = ({ item }: { item: PageProps }) => (
+  <PageThumbnail page={item} />
+);
 
 export function ThumbnailSlider({ pages }: { pages: Array<PageProps> }) {
   return (
-    <View style={{ flexDirection: "row" }}>
-      {pages.map((page) => (
-        <PageThumbnail page={page}></PageThumbnail>
-      ))}
-    </View>
+    <FlatList<PageProps>
+      data={pages}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.pagePosition}
+      horizontal={true}
+    />
+
+    // <View style={{ flexDirection: "row" }}>
+    //   {pages.map((page) => (
+    //     <PageThumbnail page={page}></PageThumbnail>
+    //   ))}
+    // </View>
   );
 }
