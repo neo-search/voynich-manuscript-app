d7bfebc31d8afc55cebe4a9daa477e1650878122
Daniel Beck <d.danielbeck@googlemail.com>
2021-10-30 23:26:58 +0200

    Layout Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,8 +11,8 @@ import "../translations/i18n";
 
 // const ICON_COLOR = "#B96F58";
 // const ICON_COLOR = "#6D8D68";
-const ICON_COLOR = "white";
-const TEXT_COLOR = "white";
+const ICON_COLOR = "#494949";
+const TEXT_COLOR = "#494949";
 
 const Hero = () => {
   const { t } = useTranslation();
@@ -20,17 +20,10 @@ const Hero = () => {
     <View
       style={{
         borderRadius: 14,
-        // borderWidth: 4,
-        // fontSize: 18,
-        // fontFamily: "Gill Sans",
-        // textAlign: "center",
-        // margin: 10,
         margin: 0,
         paddingVertical: 16,
         flexDirection: "row",
-        // border:12
-        // color: "#ffffff",
-        backgroundColor: "#B96F58",
+        // backgroundColor: "#B96F58",
         flexWrap: "wrap",
       }}
     >
@@ -58,7 +51,25 @@ const Hero = () => {
             // fontStyle: "italic",
           }}
         >
-          {t("history")}
+          {t("FAQ")}
+        </Text>
+      </View>
+      <View style={{ alignContent: "center", marginLeft: 12, width: 70 }}>
+        {/* <FontAwesome
+          name="university"
+          style={{ textAlign: "center" }}
+          size={32}
+          color="#A18F6A"
+        /> */}
+        <FontAwesome
+          name="reddit"
+          style={{ textAlign: "center" }}
+          size={40}
+          color={ICON_COLOR}
+        />
+        {/* <Ionicons name="md-checkmark-circle" size={32} color="green" /> */}
+        <Text style={{ color: TEXT_COLOR, textAlign: "center", marginTop: 5 }}>
+          {t("reddit_forum")}
         </Text>
       </View>
       <View style={{ alignContent: "center", marginLeft: 12, width: 70 }}>
@@ -78,75 +89,7 @@ const Hero = () => {
         />
         {/* <Ionicons name="md-checkmark-circle" size={32} color="green" /> */}
         <Text style={{ color: TEXT_COLOR, textAlign: "center", marginTop: 5 }}>
-        {t("faq")}
-        </Text>
-      </View>
-      <View style={{ alignContent: "center", marginLeft: 12, width: 70 }}>
-        {/* <FontAwesome
-          name="university"
-          style={{ textAlign: "center" }}
-          size={32}
-          color="#A18F6A"
-        /> */}
-        <FontAwesome
-          name="reddit"
-          style={{ textAlign: "center" }}
-          size={40}
-          color={ICON_COLOR}
-        />
-        {/* <Ionicons name="md-checkmark-circle" size={32} color="green" /> */}
-        <Text style={{ color: TEXT_COLOR, textAlign: "center", marginTop: 5 }}>
-        {t("reddit_forum")}
-        </Text>
-      </View>
-      <View style={{ alignContent: "center", marginLeft: 12, width: 70 }}>
-        {/* <FontAwesome
-          name="university"
-          style={{ textAlign: "center" }}
-          size={32}
-          color="#A18F6A"
-        /> */}
-        <SimpleLineIcons
-          name="note"
-          style={{ textAlign: "center" }}
-          size={40}
-          color={ICON_COLOR}
-        />
-        {/* <FontAwesome
-          name="reddit"
-          style={{ textAlign: "center" }}
-          size={40}
-          color="#A18F6A"
-        /> */}
-        {/* <Ionicons name="md-checkmark-circle" size={32} color="green" /> */}
-        <Text style={{ color: TEXT_COLOR, textAlign: "center", marginTop: 5 }}>
-          {t("my_notes")}
-        </Text>
-      </View>
-      <View style={{ alignContent: "center", marginLeft: 12, width: 70 }}>
-        {/* <FontAwesome
-          name="university"
-          style={{ textAlign: "center" }}
-          size={32}
-          color="#A18F6A"
-        /> */}
-
-        <AntDesign
-          name="heart"
-          style={{ textAlign: "center" }}
-          size={40}
-          color={ICON_COLOR}
-        />
-        {/* <Ionicons name="md-checkmark-circle" size={32} color="green" /> */}
-        <Text
-          style={{
-            // color: ICON_COLOR,
-            textAlign: "center",
-            marginTop: 5,
-            color: TEXT_COLOR,
-          }}
-        >
-          {t("premium")}
+          {t("Imprint")}
         </Text>
       </View>
     </View>
